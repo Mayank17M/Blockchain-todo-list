067f521bad636ad65ece325f6992a1cbeb8611ae
Mayank17M <mamgain.mayank01@gmail.com>
2021-09-01 18:15:00 +0530

    Create first task

--- contracts/todoList.sol
+++ contracts/todoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract todoList{
     uint public taskCount= 0;
+
+    struct task{
+        uint id;
+        string content;
+        bool isComplete; 
+    }
+
+    mapping(uint => task) public tasks;
+
+    constructor() public{
+        createTask("Check out github.com/Mayank17M");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = task(taskCount, _content, false);
+    }
 }
