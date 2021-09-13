7e7a3602c7b0c31d560345bbd396f903a1859889
Mayank17M <mamgain.mayank01@gmail.com>
2021-09-14 01:29:11 +0530

    Add test

--- contracts/todoList.sol
+++ contracts/todoList.sol
@@ -3,21 +3,38 @@ pragma solidity ^0.5.0;
 contract todoList{
     uint public taskCount= 0;
 
-    struct task{
+    struct Task {
         uint id;
         string content;
-        bool isComplete; 
+        bool completed;
     }
-
-    mapping(uint => task) public tasks;
 
     constructor() public{
         createTask("Check out github.com/Mayank17M");
     }
 
+    mapping(uint => Task) public tasks;
+        event TaskCreated(
+        uint id,
+        string content, 
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     function createTask(string memory _content) public {
         taskCount++;
-        tasks[taskCount] = task(taskCount, _content, false);
+        tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
